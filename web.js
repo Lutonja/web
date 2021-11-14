2869c1d85c0c370e5a8d3dd1d01fb191956979ff
Lutonja <46675455+Lutonja@users.noreply.github.com>
2021-11-14 12:43:59 +0300

    Update web.js

--- web.js
+++ web.js
@@ -1,3 +1,29 @@
+setInterval(connected, 1000);
+
+function connected(){
+    switch (navigator.onLine){
+        case true:
+            $("#on").fadeOut(1000);
+                $("#on").css("background-color", "green");
+                $("#st").css("background-color", "rgb(217, 245, 215)");
+                $("#on").fadeIn(1000);
+                document.getElementById("status").innerHTML = "online";
+                break;
+        case false:
+            $("#on").fadeOut(1000);
+                $("#on").css("background-color", "red");
+                $("#on").fadeIn(1000);
+                $("#st").css("background-color", "rgb(245, 224, 215)");
+                document.getElementById("status").innerHTML = "offline";
+                break;
+        default:
+            $("#on").fadeOut(1000);
+                $("#on").css("background-color", "rgb(148, 147, 147)");
+                $("#on").fadeIn(1000);
+                $("#st").css("background-color", "rgb(148, 147, 147)");
+                document.getElementById("status").innerHTML = "status";
+    }
+}
 
     $("#leo").click(function(){
         alert("Contents will disappear");
